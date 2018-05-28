19baa74455d760ca943525b4c65a326be0875ed8
Heidi Hlavinka <hchlav@gmail.com>
2018-05-28 23:22:39 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
     elsif
       !valid?
         self.status = "rejected"
-        "Transaction rejected. Please check your account balance."
+        # "Transaction rejected. Please check your account balance."
         binding.pry
     end
   end
