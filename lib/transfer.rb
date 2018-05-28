bf251d9869b5672f245f17ec963a2d9bd1f95677
Heidi Hlavinka <hchlav@gmail.com>
2018-05-28 23:14:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,9 +17,9 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && @sender.balance > amount && self.status = "pending"
-      @sender.balance -= amount
-      @receiver.balance += amount
+    if valid? && sender.balance > amount && self.status = "pending"
+      sender.balance -= amount
+      receiver.balance += amount
       self.status = "complete"
     end
 
